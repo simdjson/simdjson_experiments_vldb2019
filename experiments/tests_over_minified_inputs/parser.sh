8dbd6d7e9cceddbe9b6a79b81f37f9d931294c3f
Daniel Lemire <lemire@gmail.com>
2019-05-29 15:23:12 -0400

    Tweaks.

--- experiments/tests_over_minified_inputs/parser.sh
+++ experiments/tests_over_minified_inputs/parser.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH/../../library/simdjson
-make -k parsingcompetition minify jsonstats
 echo 
 for i in jsonexamples/*.json; do
     [ -f "$i" ] || break
-    echo -n "#"
     echo $i
-    echo -n "#"
-    ./jsonstats $i
     ./parsingcompetition    $i
-    ./parsingcompetition -t 
     echo "# minifying"
     ./minify $i > $i.minified
-    echo -n "#"
     echo $i.minified
-    echo -n "#"
-    ./jsonstats $i.minified
     ./parsingcompetition  $i.minified
-    ./parsingcompetition -t  $i.minified
-     echo
-    echo "# ========"
+    echo
     echo
 done
