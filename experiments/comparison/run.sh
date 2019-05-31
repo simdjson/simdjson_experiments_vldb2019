bae8eafd917a6b121bc4f31265156390e4fd2037
Daniel Lemire <lemire@gmail.com>
2019-05-31 15:54:50 -0400

    New experiments.

---
+++ experiments/comparison/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RESULTSPATH=/results #hardcoded
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH/../../library/simdjson
+for i in jsonexamples/*.json; do
+    [ -f "$i" ] || break
+    shortname=$(basename $i .json).table
+    ./parsingcompetition -t $i > $RESULTSPATH/$shortname
+    ./allparsingcompetition -t $i > $RESULTSPATH/all$shortname
+    echo -n "."
+done
+echo
