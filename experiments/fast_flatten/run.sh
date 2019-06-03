59961041084d40c49da49ec7fb597c037dfba869
Daniel Lemire <lemire@gmail.com>
2019-06-03 14:29:07 -0400

    Now with fast-flatten experiments.

---
+++ experiments/fast_flatten/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RESULTSPATH=/results #hardcoded
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH/../../library/simdjson
+fastresult=$RESULTSPATH/fastflatten.txt
+slowresult=$RESULTSPATH/slowflatten.txt
+echo -n "" > $fastresult
+echo -n "" > $slowresult
+for i in jsonexamples/*.json; do
+    [ -f "$i" ] || break
+    echo "#"$i >> $fastresult
+    ./fastparse -t $i >> $fastresult
+    echo "#"$i >> $slowresult
+    ./slowparse -t $i >> $slowresult
+    echo -n "."
+done
+echo
